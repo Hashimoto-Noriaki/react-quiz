8544a9936a361269506b53acfa72fc1895899290
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-06-21 01:37:26 +0900

    クイズの問題を作成

--- src/components/pages/QuizPage.tsx
+++ src/components/pages/QuizPage.tsx
@@ -1,13 +1,19 @@
 import Display  from '../Display/Display';
-import quizDate  from '../../data/quiz';
+import quizData  from '../../data/quiz';
+import Button from '../Button/Button';
 
 export default function QuizPage()  {
     const quizIndex = 0;
     return (
         <>
             <Display>
-                {`Q1. ${quizDate[quizIndex].question}`}
+                {`Q1. ${quizData[quizIndex].question}`}
             </Display>
+            <Button>{quizData[quizIndex].options[0]}</Button>
+            <Button>{quizData[quizIndex].options[1]}</Button>
+            <Button>{quizData[quizIndex].options[2]}</Button>
+            <Button>{quizData[quizIndex].options[3]}</Button>
+            <Button>{quizData[quizIndex].options[4]}</Button>
         </>
     )
 }
