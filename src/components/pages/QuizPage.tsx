abc6fbe6961a00f8204c136d1cdd54593eda285b
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-06-21 15:57:39 +0900

    クイズの正誤判定ロジックの追加

--- src/components/pages/QuizPage.tsx
+++ src/components/pages/QuizPage.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import Display  from '../Display/Display';
 import quizData  from '../../data/quiz';
 import Button from '../Button/Button';
 
 export default function QuizPage()  {
-    const quizIndex = 0;
+    const [quizIndex,setQuizIndex] = useState(0);
+    const [answerLogs,setAnswerLogs] = useState([]);//[]は空の配列。これは答えを保存しておく
     const handleClick = (clickedIndex) => {
-        console.log("clickedIndex", clickedIndex);
+        //ここは関数型更新
+        if(clickedIndex === quizData[quizIndex].answerIndex){
+            setAnswerLogs([...answerLogs((prev) => [...prev,true])]);//...はスプレッド構文 より簡単な書き方で配列に要素を追加して保存
+        } else {
+            setAnswerLogs((prev) => [...prev,false]);
+        }
+        setQuizIndex((prev) => prev + 1);//次の問題に進むための処理 現在のquizIndexに1足して保存する
     }
     return (
         <>
