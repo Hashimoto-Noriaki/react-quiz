70b69ae045208dfeb9fa568aa8dbe8f733d0aeb2
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-06-21 17:54:48 +0900

    正解数表示

--- src/components/pages/ResultPage.tsx
+++ src/components/pages/ResultPage.tsx
@@ -1,5 +1,14 @@
+import { useLocation } from 'react-router-dom';
+
 export default function ResultPage() {
+    const location = useLocation();
+    const maxQuizLen = location.state.maxQuizLen
+    const correctNumLen = location.state.correctNumLen
     return (
-        <div>Result</div>
+        <>
+            <h1>Result</h1>
+            <p>あなたの正解数は...</p>
+            <p>{`全${maxQuizLen}問中、${correctNumLen}問正解でした‼︎`}</p>
+        </>
     )
 }
