c7725855676248f619e93d05a16465fdf99e6c4a
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-06-30 18:07:01 +0900

    mapを使ってリファクタリング

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,8 @@
 import styles from './Button.module.css';
 
-export default function Button({children}: {children: React.ReactNode}) {
+export default function Button({children,onClick}: {children: React.ReactNode}) {
     return (
-        <button className={styles.button}>
+        <button className={styles.button} onClick={onClick}>
             <span className={styles.buttonInner}>{children}</span>
         </button>
     )
